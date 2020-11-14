b65d54bc728f5d1b9451949ab4918a9ddbeeeaeb
Max Rashes <max.j.rashes@pwc.com>
2020-11-13 19:27:33 -0600

    snow initalized

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Container, Row, Col, Form } from "react-bootstrap"
+// import { useState,  } from "react"
 
-function App() {
+
+const App = (props) => {
+  // const [totalPeople, setTotal] = useState(1)
+  // const [people] = useState([{name: "", email: ""}])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form>
+      <Container className="santa-wrapper">
+        <Row>
+          <Col className="content">
+            <h1 className="text-center">Secret Santa</h1>
+            {/* {people.map(el => <label>Name: <input value={el.name}/></label>)} */}
+            
+            {/* <button type="button" onClick={() => setTotal(totalPeople+1)}>Add New Person</button> */}
+          </Col>
+        </Row>
+      </Container>
+    </Form>
   );
 }
 
